9d1c17e4c9793064a5bd147cd2e95900ecf015b0
Kanishka Kanungo <kanungokanishka@gmail.com>
2024-04-03 22:38:16 +0530

    Leetcode (Easy) Question

---
+++ Heights.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count = 0;
+        
+        int[] arr = Arrays.copyOf(heights,heights.length);
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=heights[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
